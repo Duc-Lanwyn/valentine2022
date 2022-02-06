85a19b0e10dca890173e33ef182dcc22122bb1c7
Duc Lanwyn <stian.nlm@gmail.com>
2022-02-06 16:57:37 -0500

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'valentine-2022';
 
-  polkovodets_rumyantsev = "DEILAOR";
+  polkovodets_rumyantsev_key = "DEILAOR".split('');
+
+  polkovodets_rumyantsev: any[] = [
+    {
+      secretKey: "D",
+      backgroundClass: "pic1",
+      hint: "Many a fascists have been trampled under this war machine"
+    },
+    {
+      secretKey: "E",
+      backgroundClass: "pic2",
+      hint: "Bring me my red bag with my makeuppee!!!"
+    },
+    {
+      secretKey: "I",
+      backgroundClass: "pic3",
+      hint: "Kuhu ma lähen esimesena iga päev kui arkan (ei, mitte vannituba)"
+    },
+    {
+      secretKey: "L",
+      backgroundClass: "pic4",
+      hint: "Last letter of the middle word under two gray flags"
+    },
+    {
+      secretKey: "A",
+      backgroundClass: "pic5",
+      hint: "Raamat"
+    },
+    {
+      secretKey: "O",
+      backgroundClass: "pic6",
+      hint: "Nina Inhalaator (pic)"
+    },
+    {
+      secretKey: "R",
+      backgroundClass: "",
+      hint: "Ask a fortune telling paper"
+    }
+  ];
 
   ngOnInit() {
-    
+  }
+
+  onCardSolved( config: any) {
+    config.solved = true;
   }
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  mode: "hint" | "solution" = "hint";
+
+  solved: boolean = false;
+
+  @Input() 
+  secretKey: string = "";
+  
+  @Input()
+  customBackgroundClass: string = "";
+
+  @Input()
+  hint: string = "";
+
+  @Output() onSolved = new EventEmitter<boolean>();
+  
+
+  ngOnInit(): void {
+  }
+
+  toggleMode() {
+    this.mode = this.mode === "hint" ? "solution" : "hint";
+  }
+
+  onSolutionInputClick( event: MouseEvent ) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onSolutionInput(event: KeyboardEvent) {
+    if ( (event.target as HTMLInputElement).value.toUpperCase() === this.secretKey ) {
+      this.solved = true;
+      this.onSolved.emit(true);
+    }
+  }
+
+}
